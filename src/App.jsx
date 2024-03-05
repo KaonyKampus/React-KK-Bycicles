7f9ecf98b2460433f99796b5ca0d7c3a6fbaef69
David Silva <davidsilvav6@gmail.com>
2024-03-04 23:52:25 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import NavBar from './components/NavBar.jsx'
 import ItemListContainer from './components/ItemListContainer.jsx'
 
+
 function App () {
 
 
   return (
     <>
     <NavBar />
-    <ItemListContainer greeting='productos a la venta' />
+
+    <ItemListContainer greeting="Productos a la venta"/>
+    
     </>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,11 @@
 import '../items.css'
 
-
 export default function ItemListContainer({greeting}){
-
     return(
         <>
-        <section className="itemsContainer">
-            <h2>{greeting}</h2>
-        </section>
+           <section className='itemsContainer'>
+             <h2>{greeting}</h2>
+           </section>  
         </>
-    )
+    );
 }
