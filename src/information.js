771f96ce219432a016b02c36276dea775441914e
David Silva <davidsilvav6@gmail.com>
2024-03-14 18:48:24 -0500

    Arrays

---
+++ src/information.js
@@ -0,0 +1,65 @@
+
+const BicicletasRuta = [
+
+    {id: 000001, nombre:"Bicicleta Ruta 1", precio:2000000, imagen:"./assets/card-images/ruta1.jpg"},
+    {id: 000002, nombre:"Bicicleta Ruta 2", precio:2000000, imagen:"./assets/card-images/ruta2.jpg"},
+    {id: 000003, nombre:"Bicicleta Ruta 3", precio:2000000, imagen:"./assets/card-images/ruta3.jpg"},
+    {id: 000004, nombre:"Bicicleta Ruta 4", precio:2000000, imagen:"./assets/card-images/ruta4.jpg"},
+    {id: 000005, nombre:"Bicicleta Ruta 5", precio:2000000, imagen:"./assets/card-images/ruta5.jpg"},
+    {id: 000006, nombre:"Bicicleta Ruta 6", precio:2000000, imagen:"./assets/card-images/ruta6.jpg"},
+    {id: 000007, nombre:"Bicicleta Ruta 7", precio:2000000, imagen:"./assets/card-images/ruta7.jpg"},
+    {id: 000008, nombre:"Bicicleta Ruta 8", precio:2000000, imagen:"./assets/card-images/ruta8.jpg"},
+    {id: 000009, nombre:"Bicicleta Ruta 9", precio:2000000, imagen:"./assets/card-images/ruta9.jpg"},
+    {id: 000010, nombre:"Bicicleta Ruta 10", precio:2000000, imagen:"./assets/card-images/ruta10.jpg"},
+    {id: 000011, nombre:"Bicicleta Ruta 11", precio:2000000, imagen:"./assets/card-images/ruta11.jpg"},
+]
+
+
+
+const BicicletasMontana = [
+
+    {id: 100001, nombre:"Bicicleta Montana 1", precio:2000000, imagen:"./assets/card-images/montana1.jpg"},
+    {id: 100002, nombre:"Bicicleta Montana 2", precio:2000000, imagen:"./assets/card-images/montana2.jpg"},
+    {id: 100003, nombre:"Bicicleta Montana 3", precio:2000000, imagen:"./assets/card-images/montana3.jpg"},
+    {id: 100004, nombre:"Bicicleta Montana 4", precio:2000000, imagen:"./assets/card-images/montana4.jpg"},
+    {id: 100005, nombre:"Bicicleta Montana 5", precio:2000000, imagen:"./assets/card-images/montana5.jpg"},
+    {id: 100006, nombre:"Bicicleta Montana 6", precio:2000000, imagen:"./assets/card-images/montana6.jpg"},
+    {id: 100007, nombre:"Bicicleta Montana 7", precio:2000000, imagen:"./assets/card-images/montana7.jpg"},
+    {id: 100008, nombre:"Bicicleta Montana 8", precio:2000000, imagen:"./assets/card-images/montana8.jpg"},
+    {id: 100009, nombre:"Bicicleta Montana 9", precio:2000000, imagen:"./assets/card-images/montana9.jpg"},
+    {id: 100010, nombre:"Bicicleta Montana 10", precio:2000000, imagen:"./assets/card-images/montana10.jpg"},
+    {id: 100011, nombre:"Bicicleta Montana 11", precio:2000000, imagen:"./assets/card-images/montana11.jpg"},
+    {id: 100012, nombre:"Bicicleta Montana 12", precio:2000000, imagen:"./assets/card-images/montana12.jpg"},
+    {id: 100013, nombre:"Bicicleta Montana 13", precio:2000000, imagen:"./assets/card-images/montana13.jpg"},
+]
+
+
+const BicicletasGravel = [
+
+    {id: 200001, nombre:"Bicicleta Gravel 1", precio:2000000, imagen:"./assets/card-images/montana1.jpg"},
+    {id: 200002, nombre:"Bicicleta Gravel 2", precio:2000000, imagen:"./assets/card-images/montana2.jpg"},
+    {id: 200003, nombre:"Bicicleta Gravel 3", precio:2000000, imagen:"./assets/card-images/montana3.jpg"},
+    {id: 200004, nombre:"Bicicleta Gravel 4", precio:2000000, imagen:"./assets/card-images/montana4.jpg"},
+    {id: 200005, nombre:"Bicicleta Gravel 5", precio:2000000, imagen:"./assets/card-images/montana5.jpg"},
+    {id: 200006, nombre:"Bicicleta Gravel 6", precio:2000000, imagen:"./assets/card-images/montana6.jpg"},
+    {id: 200007, nombre:"Bicicleta Gravel 7", precio:2000000, imagen:"./assets/card-images/montana7.jpg"},
+]
+
+const BicicletaMontana = [
+
+    {id: 100001, nombre:"Bicicleta Montana 1", precio:2000000, imagen:"./assets/card-images/montana1.jpg"},
+    {id: 100002, nombre:"Bicicleta Montana 2", precio:2000000, imagen:"./assets/card-images/montana2.jpg"},
+    {id: 100003, nombre:"Bicicleta Montana 3", precio:2000000, imagen:"./assets/card-images/montana3.jpg"},
+    {id: 100004, nombre:"Bicicleta Montana 4", precio:2000000, imagen:"./assets/card-images/montana4.jpg"},
+    {id: 100005, nombre:"Bicicleta Montana 5", precio:2000000, imagen:"./assets/card-images/montana5.jpg"},
+    {id: 100006, nombre:"Bicicleta Montana 6", precio:2000000, imagen:"./assets/card-images/montana6.jpg"},
+    {id: 100007, nombre:"Bicicleta Montana 7", precio:2000000, imagen:"./assets/card-images/montana7.jpg"},
+    {id: 100008, nombre:"Bicicleta Montana 8", precio:2000000, imagen:"./assets/card-images/montana8.jpg"},
+    {id: 100009, nombre:"Bicicleta Montana 9", precio:2000000, imagen:"./assets/card-images/montana9.jpg"},
+    {id: 100010, nombre:"Bicicleta Montana 10", precio:2000000, imagen:"./assets/card-images/montana10.jpg"},
+
+]
+
+
+
+
